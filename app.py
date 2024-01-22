b234d8620f516c437ad4d778e22cc46e1b80626e
Himansu <himansusahoo@gmail.com>
2024-01-21 23:04:00 -0500

    added app.py, requirements.txt

---
+++ app.py
@@ -0,0 +1,70 @@
+import streamlit as st
+import google.generativeai as genai 
+import os
+import sys
+import PyPDF2 as pdf 
+
+from dotenv import load_dotenv
+
+load_dotenv() ## load all environmental variables
+print("Google api key : ", os.environ.get("GOOGLE_API_KEY"))
+
+genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
+
+## Gemini Pro Response
+
+def get_gemini_response(input):
+    model = genai.GenerativeModel("gemini-pro")
+    response = model.generate_content(input)
+    return response.text
+
+# read the pdf to extract text
+def input_pdf_text(uploaded_file):
+    reader = pdf.PdfReader(uploaded_file)
+    text = ""
+    for page in reader(len(reader.pages)):
+        page=reader.pages[page]
+        text += str(page.extract_text())
+    return text
+
+
+input_prompt="""
+Hey Act Like a skilled or very experience ATS(Application Tracking System)
+with a deep understanding of tech field,software engineering,data science ,data analyst
+and big data engineer. Your task is to evaluate the resume based on the given job description.
+You must consider the job market is very competitive and you should provide 
+best assistance for improving thr resumes. Assign the percentage Matching based 
+on Jd and
+the missing keywords with high accuracy
+resume:{text}
+description:{jd}
+
+I want the response in one single string having the structure
+{{"JD Match":"%","MissingKeywords:[]","Profile Summary":""}}
+"""
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
